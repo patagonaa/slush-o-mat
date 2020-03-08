31290d8d38988c5d935bee0bb24285c359752c85
Patrick Krämer <patagonahn@gmail.com>
2020-03-08 19:11:56 +0100

    add icon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.ts",
@@ -28,6 +29,14 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./src/index.html"
-        })
-    ]
+        }),
+        new CopyWebpackPlugin([
+            {from:'./src/icon.png'}
+        ])
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 8080
+    }
 };
