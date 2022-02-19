9cb54a1d40f2bd941f8f67315f01c74916c2d529
Patrick Krämer <patagonahn@gmail.com>
2022-02-19 03:11:01 +0100

    upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         ]})
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'src'),
+        static: path.join(__dirname, 'src'),
         compress: true,
         port: 8080
     }
