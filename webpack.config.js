e3625a04d10e71e595f8411fac4d43543c41548a
Patrick <patagonahn@gmail.com>
2023-02-14 10:56:17 +0100

    fix: script caching

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: "./src/index.ts",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].js",
+        filename: "[name].[contenthash].js",
         publicPath: "/"
     },
     resolve: {
